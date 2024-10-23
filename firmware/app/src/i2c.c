7152ac45a8ffed7f0d05affe22c800bc8772dbe2
chris <tps90067@gmail.com>
2024-10-23 18:06:46 -0400

    WIP start populating i2c and pca9685 driver

--- firmware/app/src/i2c.c
+++ firmware/app/src/i2c.c
@@ -1,11 +1,17 @@
 #include "i2c.h"
 #include "esp_err.h"
+#include "freertos/FreeRTOS.h"
 
 #define I2C_EXAMPLE_MASTER_SCL_IO           2
 #define I2C_EXAMPLE_MASTER_SDA_IO           14
 #define I2C_EXAMPLE_MASTER_NUM              I2C_NUM_0
 #define I2C_ENABLE_SDA_PULLUP               0
 #define I2C_ENABLE_SCL_PULLUP               0
+#define WRITE_BIT                           I2C_MASTER_WRITE
+#define READ_BIT                            I2C_MASTER_READ
+#define ACK_CHECK_EN                        0x1
+#define ACK_CHECK_DIS                       0x0
+#define LAST_NACK_VAL                       0x2              /*!< I2C last_nack value */
 #define I2C_CLK_STRETCH_TICK                300 // 300 ticks, Clock stretch is about 210us, you can make changes according to the actual situation.
 
 esp_err_t i2c_master_init(void)
@@ -21,4 +27,56 @@ esp_err_t i2c_master_init(void)
     ESP_ERROR_CHECK(i2c_driver_install(i2c_master_port, conf.mode));
     ESP_ERROR_CHECK(i2c_param_config(i2c_master_port, &conf));
     return ESP_OK;
+}
+
+esp_err_t i2c_readReg(uint8_t addr, uint8_t regAddr, uint8_t* data, size_t dataLen)
+{
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, regAddr, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr << 1 | READ_BIT, ACK_CHECK_EN);
+    i2c_master_read(cmd, data, dataLen, LAST_NACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+esp_err_t i2c_writeReg(uint8_t addr, uint8_t regAddr, uint8_t* data, size_t dataLen)
+{
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, regAddr, ACK_CHECK_EN);
+    i2c_master_write(cmd, data, dataLen, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+esp_err_t i2c_readReg8(uint8_t addr, uint8_t regAddr, uint8_t* data)
+{
+    return i2c_readReg(addr, regAddr, data, 1);
+}
+
+
+esp_err_t i2c_writeReg8(uint8_t addr, uint8_t regAddr, uint8_t* data)
+{
+    return i2c_writeReg(addr, regAddr, data, 1);
 }
--- firmware/app/src/pca9685.c
+++ firmware/app/src/pca9685.c
@@ -1,1 +1,32 @@
 #include "pca9685.h"
+#include "i2c.h"
+
+esp_err_t pca9685_setPrescaler(PCA9685_t* pca9685, uint8_t prescaler);
+uint8_t pca9685_getPrescaler(PCA9685_t* pca9685);
+void pca9685_setPWM(PCA9685_t* pca9685, uint8_t outputPin,uint16_t onPos, uint16_t offPos);
+void pca9685_getPWM(PCA9685_t* pca9685, uint8_t outputPin, bool getOff);
+
+void pca9685_setPrescaler(PCA9685_t* pca9685, uint8_t prescaler)
+{
+    esp_err_t ret = ESP_OK;
+    const ADDR = pca9685->addr;
+
+    uint8_t oldMode = 0;
+    ret &= i2c_readReg8(ADDR, PCA9685_MODE1, &oldMode);
+
+    uint8_t newMode = (oldmode & ~MODE1_RESTART) | MODE1_SLEEP; // sleep
+    ret &= i2c_writeReg8(ADDR, PCA9685_MODE1, &newMode);
+    ret &= i2c_writeReg8(ADDR, PCA9685_PRESCALE, &prescaler);
+    ret &= i2c_writeReg8(ADDR, PCA9685_MODE1, &oldMode);
+    
+    return ret;
+}
+
+uint8_t pca9685_getPrescaler(PCA9685_t* pca9685)
+{
+    uint8_t prescaler = 0;
+    i2c_writeReg8(ADDR, PCA9685_PRESCALE, &prescaler);
+
+
+    return prescaler;
+}
