505377e0a4eaa91e8a94770c1f99753558577979
chris <tps90067@gmail.com>
2024-10-24 09:52:03 -0400

    WIP

--- firmware/app/src/pca9685.c
+++ firmware/app/src/pca9685.c
@@ -1,5 +1,6 @@
 #include "pca9685.h"
 #include "i2c.h"
+#include "math.h"
 
 #define ON_L_OFFSET     0
 #define ON_H_OFFSET     1
@@ -9,6 +10,17 @@
 #define ON_OFF_H_MASK   0x0F00
 #define SET_PWM_SIZE    4
 #define GET_PWM_SIZE    2
+
+#define MIN_FREQ        24.0F
+#define MAX_FREQ        1526.0F
+
+#define MIN_POS         0.0F   // 0% duty
+#define MAX_POS         255.0F //100% duty
+#define MIN_OFF_POS     203.8F // 5% duty
+#define MAX_OFF_POS     409.0F // 10% duty
+#define GAIN            ((MAX_OFF_POS - MIN_OFF_POS)/(MAX_POS - MIN_POS))
+
+#define TOTAL_NUM_SERVO 15
 
 void        pca9685_setPrescaler(PCA9685_t* pca9685, uint8_t prescaler);
 uint8_t     pca9685_getPrescaler(PCA9685_t* pca9685);
@@ -70,25 +82,43 @@ uint16_t pca9685_getPWM(PCA9685_t* pca9685, uint8_t outputPin, bool getOff)
 
 
 // init
-void PCA9685_init(void)
+void PCA9685_init(PCA9685_t* pca9685)
 {
+    //configure the mode 1
 
+    //configure mode 2
+
+    //set the frequency 
 }
 
-// set frequency
-void PCA9685_setFreq(uint16_t freq)
+// set frequency between 24Hz and 1526Hz
+void PCA9685_setFreq(PCA9685_t* pca9685, float freq)
 {
+    if (freq < MIN_FREQ){freq = MIN_FREQ;}
+    if (freq > MAX_FREQ){freq = MAX_FREQ;}
 
+    uint8_t prescaler = (uint16_t)(round(pca9685->osc_freq / (4096.0 * freq)) - 1.0);
+
+    pca9685_setPrescaler(pca9685, prescaler);
 }
 
 // set one servo position
-void PCA9685_setServoPos(uint8_t pos)
+void PCA9685_setServoPos(PCA9685_t* pca9685, uint8_t outputPin, uint8_t servoPos)
 {
+    const uint16_t ON_POS = 0;
+    const uint16_t OFF_POS = (uint16_t)round(GAIN * servoPos + MIN_OFF_POS);
 
+    pca9685_setPWM(pca9685, outputPin, ON_POS, OFF_POS);
 }
 
 // set all servo position to the same
-void PCA9685_setAllServoPos(uint8_t pos)
+void PCA9685_setAllServoPos(PCA9685_t* pca9685, uint8_t servoPos)
 {
+    const uint16_t ON_POS = 0;
+    const uint16_t OFF_POS = (uint16_t)round(GAIN * servoPos + MIN_OFF_POS);
     
+    for (uint8_t i = 0; i < TOTAL_NUM_SERVO; i++)
+    {
+        pca9685_setPWM(pca9685, i, ON_POS, OFF_POS);
+    }
 }
