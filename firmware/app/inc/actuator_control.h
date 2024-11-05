699028b714e3aa0695c70898603b31477f024b83
chris <tps90067@gmail.com>
2024-11-05 15:33:24 -0500

    WIP, does not compile

--- firmware/app/inc/actuator_control.h
+++ firmware/app/inc/actuator_control.h
@@ -1,6 +1,10 @@
 #ifndef ACTUATOR_CONTROL_H
 #define ACTUATOR_CONTROL_H
 
+#include "stdint.h"
+
 #define NUM_ACTUATORS 45
 
+void updateActuatorPositions(uint8_t* positionArray);
+void resetAcuatorPositions(void);
 #endif
