a78c03907402e6fea1c8c11039fd1e10733ba698
chris <tps90067@gmail.com>
2024-11-12 15:45:07 -0500

    WIP, continue implementing state machine

--- firmware/app/src/actuator_control.c
+++ firmware/app/src/actuator_control.c
@@ -1,1 +1,57 @@
 #include "actuator_control.h"
+
+typedef enum {
+    RESET = 0,
+    STATIC,
+    DYNAMIC_PRESET_1,
+    DYNAMIC_PRESET_2,
+    DYNAMIC_PRESET_3,
+    RANDOM
+} ActControlScheme_e;
+
+typedef struct {
+    
+    ActControlScheme_e controlScheme;
+    uint8_t actPositions[NUM_ACTUATORS];
+
+} ActControl_t;
+
+ActControl_t actControl;
+
+void select_actuator_control_scheme(void)
+{
+
+}
+
+void calculate_actuator_positions(void)
+{
+
+}
+
+void apply_actuator_positions(void)
+{
+
+}
+
+void run_actuator_control_task(void)
+{
+    /**
+     * Step 1: Mode select
+     *  - determines the motor control scheme
+     * 
+     * Step 2: Calculate actuator positions
+     *  - based on the selected mode, calculate the actuator positions and populate the buffer
+     *  - must consider the time it takes to most the servo motors in percentage/sec
+     * 
+     * Step 3: Apply actuator positions
+     *  - call the pwm driver for the motors
+     */
+
+    select_actuator_control_scheme();
+
+    calculate_actuator_positions();
+
+    apply_actuator_positions();
+
+    //delay for some time
+}
--- firmware/app/src/ball_queue.c
+++ firmware/app/src/ball_queue.c
@@ -1,1 +1,38 @@
 #include "ball_queue.h"
+
+typedef enum {
+    IDLE = 0,
+    WAITING,
+    DISPENSING,
+    FAILED
+} BallQueueState_e;
+
+BallQueueState_e state = IDLE;
+
+void run_ball_queue_task(void)
+{
+    switch (state)
+    {
+        case IDLE:
+            //do nothing
+            state = WAITING;
+            break;
+        
+        case WAITING:
+
+            //if prox sensor activated
+            state = DISPENSING;
+            break;
+        
+        case DISPENSING:
+            //dispensing sensor activated
+            state = WAITING;
+
+            //5 second timeout
+            state = FAILED;
+            break;
+        
+        case FAILED:
+            break;
+    }
+}
