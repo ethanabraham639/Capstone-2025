dd3b675fc81de66167a831bb69490ebf4e94d647
chris <tps90067@gmail.com>
2024-11-17 21:20:36 -0500

    move gpio defines

--- firmware/app/src/gpio.c
+++ firmware/app/src/gpio.c
@@ -3,21 +3,6 @@
 
 #define PIN_MASK(PIN_NUM) (1 << (PIN_NUM))
 
-#define ADC_MUX_A0_GPIO      GPIO_NUM_16
-#define ADC_MUX_A1_GPIO      GPIO_NUM_12
-#define ADC_MUX_A2_GPIO      GPIO_NUM_13
-
-#define LED0_GPIO           GPIO_NUM_5
-#define LED1_GPIO           GPIO_NUM_4
-#define LED2_GPIO           GPIO_NUM_9
-
-#define BIH_LS_GPIO         GPIO_NUM_8
-#define BD_LS_GPIO          GPIO_NUM_6
-#define BQ_LS_GPIO          GPIO_NUM_1
-
-#define GUTTER1_GPIO        GPIO_NUM_7
-#define GUTTER2_GPIO        GPIO_NUM_10
-
 gpio_config_t gpios[] = 
 {
     /*Flashing*///   {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_0),  .pull_down_en = 0, .pull_up_en = 0},
