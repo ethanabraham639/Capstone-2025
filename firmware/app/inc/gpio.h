299acae61579ce81c97c059bf3b23058e1a15ea4
chris <tps90067@gmail.com>
2024-11-17 21:20:36 -0500

    move gpio defines

--- firmware/app/inc/gpio.h
+++ firmware/app/inc/gpio.h
@@ -1,6 +1,23 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+#include "driver/gpio.h"
+
+#define ADC_MUX_A0_GPIO      GPIO_NUM_16
+#define ADC_MUX_A1_GPIO      GPIO_NUM_12
+#define ADC_MUX_A2_GPIO      GPIO_NUM_13
+
+#define LED0_GPIO           GPIO_NUM_5
+#define LED1_GPIO           GPIO_NUM_4
+#define LED2_GPIO           GPIO_NUM_9
+
+#define BIH_LS_GPIO         GPIO_NUM_8
+#define BD_LS_GPIO          GPIO_NUM_6
+#define BQ_LS_GPIO          GPIO_NUM_1
+
+#define GUTTER1_GPIO        GPIO_NUM_7
+#define GUTTER2_GPIO        GPIO_NUM_10
+
 void GPIO_init(void);
 
 #endif
