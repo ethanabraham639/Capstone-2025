751d3f269e178bf68ff81a5fd0915c4beb305352
chris <tps90067@gmail.com>
2024-11-19 15:33:03 -0500

    WIP work on actuator control

--- firmware/app/src/actuator_control.c
+++ firmware/app/src/actuator_control.c
@@ -1,4 +1,17 @@
 #include "actuator_control.h"
+#include "pca9685.h"
+
+#include "stdbool.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+#define MAX_POSITION            255.0
+#define MAX_DELAY_MS            2000.0
+#define DELTA_CONVERSION_MS     (MAX_DELAY_MS/MAX_POSITION)
+
+#define NUM_PCA9685             3
+#define NUM_ACT_PER_PCA6985     15
 
 typedef enum {
     RESET = 0,
@@ -10,48 +23,150 @@ typedef enum {
 } ActControlScheme_e;
 
 typedef struct {
-    
+    PCA9685_t pca9685[NUM_PCA9685];
     ActControlScheme_e controlScheme;
+    bool isNewPositions;
     uint8_t actPositions[NUM_ACTUATORS];
+
+
+
+    //static
+    uint8_t staticPositions[NUM_ACTUATORS];
+
+    //for all
+    uint32_t delay; //ms
 
 } ActControl_t;
 
 ActControl_t actControl;
 
-void select_actuator_control_scheme(void)
-{
 
+uint32_t calculate_delay_ms(uint8_t* pos1, uint8_t* pos2)
+{
+    //find the largest delta
+    uint8_t largestDelta = 0;
+
+    for (uint8_t i = 0; i < NUM_ACTUATORS; i++)
+    {
+        uint8_t cur_pos1 = pos1[i];
+        uint8_t cur_pos2 = pos2[i];
+
+        // subtract smaller number from bigger
+        uint8_t delta = (cur_pos1 > cur_pos2) ? (cur_pos1 - cur_pos2) : (cur_pos2 - cur_pos1);
+
+        if (delta > largestDelta)
+        {
+            largestDelta = delta;
+        }
+    }
+
+    return (uint32_t)((float)largestDelta * DELTA_CONVERSION_MS);
 }
 
-void calculate_actuator_positions(void)
-{
 
+
+
+
+
+
+
+
+
+
+void calculate_reset_positions(void)
+{
+    //set everything to 0 for now
+    actControl.delay = 0;
+    memset(actControl.actPositions, 0, NUM_ACTUATORS);
 }
+
+void calculate_static_positions(void)
+{
+    actControl.delay = calculate_delay_ms(actControl.actPositions, actControl.staticPositions);
+    memcpy(actControl.staticPositions, actControl.actPositions, NUM_ACTUATORS);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void calculate_and_set_actuator_positions(void)
+{
+    switch(actControl.controlScheme)
+    {
+        case(RESET):
+            calculate_reset_positions();
+            break;
+    
+        case(STATIC):
+            calculate_static_positions();
+            break;
+    }
+}
+
+
+
+/**
+ * - we have 45 motors
+ * - each row is 5 motors
+ * 
+ * - each i2c pwm module supports 15 motors
+ * 
+ * - lets assume this is the configuration wired up
+ * 
+ *           
+ *          35
+ * 
+ *          30
+ * 
+ *          25
+ * 
+ *          20
+ * 
+ *          15        
+ *  
+ *          10
+ * 
+ *          5   
+ *           
+ *          0   1   2   3   4
+ * 
+ */
 
 void apply_actuator_positions(void)
 {
+    for (uint8_t pca9685Index = 0; pca9685Index < NUM_PCA9685; pca9685Index++)
+    {
+        PCA9685_t* pca9685Instance = &actControl.pca9685[pca9685Index];
+
+        for (uint8_t actIndex = 0; actIndex < NUM_ACT_PER_PCA6985; actIndex++)
+        {
+            uint8_t servoPos = actControl.actPositions[pca9685Index*actIndex];
+            PCA9685_setServoPos(pca9685Instance, actIndex, servoPos);
+        }
+    }
+
 
 }
 
 void run_actuator_control_task(void)
 {
-    /**
-     * Step 1: Mode select
-     *  - determines the motor control scheme
-     * 
-     * Step 2: Calculate actuator positions
-     *  - based on the selected mode, calculate the actuator positions and populate the buffer
-     *  - must consider the time it takes to most the servo motors in percentage/sec
-     * 
-     * Step 3: Apply actuator positions
-     *  - call the pwm driver for the motors
-     */
+    calculate_and_set_actuator_positions();
 
-    select_actuator_control_scheme();
+    if (actControl.isNewPositions)
+        apply_actuator_positions();
 
-    calculate_actuator_positions();
-
-    apply_actuator_positions();
-
-    //delay for some time
+    // allow time for positions to be applied
+    vTaskDelay(actControl.delay / portTICK_RATE_MS);
 }
