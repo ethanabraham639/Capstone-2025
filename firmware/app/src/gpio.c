85befbe64e92119ea16f5444b3ae41eb9e3f9c80
Christopher Chung <tps90067@gmail.com>
2025-01-07 16:46:59 -0500

    temp changes for adc and mux testing, working

--- firmware/app/src/gpio.c
+++ firmware/app/src/gpio.c
@@ -10,17 +10,17 @@ gpio_config_t gpios[] =
     /* Mux A1 */     {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_12), .pull_down_en = 0, .pull_up_en = 0},
     /* Mux A2 */     {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_13), .pull_down_en = 0, .pull_up_en = 0},
 
-    /* LED0 */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_5),  .pull_down_en = 0, .pull_up_en = 0},
-    /* LED1 */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_4),  .pull_down_en = 0, .pull_up_en = 0},
-    /* LED2 */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_9),  .pull_down_en = 0, .pull_up_en = 0},
+    // /* LED0 */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_5),  .pull_down_en = 0, .pull_up_en = 0},
+    // /* LED1 */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_4),  .pull_down_en = 0, .pull_up_en = 0},
+    // /* LED2 */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_9),  .pull_down_en = 0, .pull_up_en = 0},
 
-    /* NULL */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_15), .pull_down_en = 0, .pull_up_en = 0},
+    // /* NULL */       {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_15), .pull_down_en = 0, .pull_up_en = 0},
 
-    /* BIH LS */     {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_8),  .pull_down_en = 0, .pull_up_en = 0},
-    /* BD LS */      {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_6),  .pull_down_en = 0, .pull_up_en = 0},
-    /* BQ_LS */      {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_1),  .pull_down_en = 0, .pull_up_en = 0},
-    /* Gutter1 LS */ {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_7),  .pull_down_en = 0, .pull_up_en = 0},
-    /* Gutter2 LS */ {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_10), .pull_down_en = 0, .pull_up_en = 0},
+    // /* BIH LS */     {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_8),  .pull_down_en = 0, .pull_up_en = 0},
+    // /* BD LS */      {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_6),  .pull_down_en = 0, .pull_up_en = 0},
+    // /* BQ_LS */      {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_1),  .pull_down_en = 0, .pull_up_en = 0},
+    // /* Gutter1 LS */ {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_7),  .pull_down_en = 0, .pull_up_en = 0},
+    // /* Gutter2 LS */ {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_10), .pull_down_en = 0, .pull_up_en = 0},
 
 
 };
--- firmware/app/src/main.c
+++ firmware/app/src/main.c
@@ -6,6 +6,8 @@
 #include "pca9685.h"
 #include "i2c.h"
 #include "wifi_init.h"
+#include "adc.h"
+#include "gpio.h"
 
 #define BLINKY_GPIO 5
 PCA9685_t led_pca9685 = {.addr = 0x64, .isLed = true, .osc_freq = 25000000.0};
@@ -13,32 +15,8 @@ PCA9685_t servo_pca9685 = {.addr = 0x61, .isLed = false, .osc_freq = 25000000.0}
 
 void init_peripherals(void)
 {
-    gpio_config_t io_conf;
-    //disable interrupt
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO15/16
-    io_conf.pin_bit_mask = 1<<BLINKY_GPIO;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
-
-    PCA9685_init(&led_pca9685);
-    PCA9685_init(&servo_pca9685);
-
-    for (uint8_t i = 1; i < 16; i++)
-    {
-        PCA9685_setServoPos(&led_pca9685, i, 0);
-    }
-
-    for (uint8_t i = 1; i < 16; i++)
-    {
-        PCA9685_setServoPos(&servo_pca9685, i, 0);
-    }
+    GPIO_init();
+    ADC_init();
 }
 
 void app_main()
@@ -63,29 +41,11 @@ void app_main()
     while(1) {
         gpio_set_level(BLINKY_GPIO, isOn);
         isOn = !isOn;
-
-        // Gradually move from 0 to 255
-        for (int pos = 0; pos <= 255; pos++)
-        {
-            for (uint8_t i = 6; i < 11; i++)
-            {
-                PCA9685_setServoPos(&servo_pca9685, i, pos); // Set servo position
-                PCA9685_setServoPos(&led_pca9685, i, pos); // Set servo position
-            }
-            vTaskDelay(20 / portTICK_PERIOD_MS); // Delay for smooth motion
-        }
-
-        // Gradually move from 255 back to 0
-        for (int pos1 = 255; pos1 > 0; pos1--)
-        {
-            for (uint8_t i = 6; i < 11; i++)
-            {
-                PCA9685_setServoPos(&servo_pca9685, i, pos1); // Set servo position
-                PCA9685_setServoPos(&led_pca9685, i, pos1); // Set servo position
-            }
-            vTaskDelay(20 / portTICK_PERIOD_MS); // Delay for smooth motion
-        }
-
+        uint8_t lv = (((float)ADC_getLvADCVal())/1023.0f) * 100;
+        uint8_t bd = (((float)ADC_getBDADCVal())/1023.0f) * 100;
+        printf("LV: %d\n", lv);
+        printf("Test: %d\n", bd);
+        vTaskDelay(250 / portTICK_PERIOD_MS); // Delay for smooth motion
     }
 
 }
