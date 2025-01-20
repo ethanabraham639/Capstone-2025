e8e15bf9660503882e75417ea5e08b4708ee1d40
chris <tps90067@gmail.com>
2025-01-20 10:30:39 -0500

    WIP, continue working on sensors

--- firmware/app/src/sensors.c
+++ firmware/app/src/sensors.c
@@ -14,6 +14,12 @@ typedef struct {
     bool ballQueue;
 } Sensors_t;
 
+Sensors_t sensors = {.ballInHole = false,
+                     .ballInGutter = false,
+                     .ballDep = false,
+                     .ballQueue = false};
+
+// All of these sensors are active LOW, so only set to true on the FALLING EDGE
 void check_ball_in_hole(void)
 {
 
@@ -43,40 +49,49 @@ void SNS_init(void)
 
 bool SNS_get_ball_in_hole(void)
 {
-
+    return sensors.ballInHole;
 }
 
 void SNS_clear_ball_in_hole(void)
 {
-
+    sensors.ballInHole = false;
 }
 
 bool SNS_get_ball_in_gutter(void)
 {
-
+    return sensors.ballInGutter;
 }
 
 void SNS_clear_ball_in_gutter(void)
 {
-
+    sensors.ballInGutter = false;
 }
 
 bool SNS_get_ball_dep(void)
 {
-
+    return sensors.ballDep;
 }
 
 void SNS_clear_ball_dep(void)
 {
-
+    sensors.ballDep = false;
 }
 
 bool SNS_get_ball_queue(void)
 {
-
+    return sensors.ballQueue;
 }
 
 void SNS_clear_ball_queue(void)
 {
+    sensors.ballQueue;
+}
 
+void SNS_run_task(void)
+{
+    check_ball_in_hole();
+    check_ball_in_gutter();
+    check_ball_dep();
+    check_ball_queue();
+    //DELAY
 }
