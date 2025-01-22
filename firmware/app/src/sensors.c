f0c714d31e1c52ab91794e32b2ca2f331373bbad
chris <tps90067@gmail.com>
2025-01-21 21:30:04 -0500

    WIP continune working on sensor, didn't try to compile yet

--- firmware/app/src/sensors.c
+++ firmware/app/src/sensors.c
@@ -1,90 +1,148 @@
 #include "sensors.h"
+#include "gpio.h"
+#include "delay.h"
 
-#define BALL_HOLE_SNS_GPIO      4
-#define GUTTER_SNS_GPIO         5
-#define BALL_DEP_SNS_GPIO       12
-#define BALL_QUEUE_SNS_GPIO     13
+#define DEBOUNCE_DELAY_MS       10
 
-#define LASER_EMITTER_GPIO      16
+typedef struct{
+    gpio_num_t gpio;
+    int confirmed_level;
+
+    bool detected;
+    bool confirmed;
+    Timer_t timer;
+} GpioSensor_t;
 
 typedef struct {
-    bool ballInHole;
-    bool ballInGutter;
-    bool ballDep;
-    bool ballQueue;
+    GpioSensor_t BIH;
+    GpioSensor_t BIG;
+    GpioSensor_t BD;
+    GpioSensor_t BQ;
 } Sensors_t;
 
-Sensors_t sensors = {.ballInHole = false,
-                     .ballInGutter = false,
-                     .ballDep = false,
-                     .ballQueue = false};
+volatile Sensors_t sensors = {.BIH.gpio = BIH_GPIO_IN, .BIH.confirmed_level = GPIO_LOW, .BIH.detected = false, .BIH.confirmed = false, .BIH.timer = 0,
+                              .BIG.gpio = BIG_GPIO_IN, .BIG.confirmed_level = GPIO_LOW, .BIG.detected = false, .BIG.confirmed = false, .BIG.timer = 0,
+                              .BD.gpio  = BD_GPIO_IN , .BD.confirmed_level  = GPIO_LOW, .BD.detected  = false, .BD.confirmed  = false, .BD.timer  = 0,
+                              .BQ.gpio  = BQ_GPIO_IN, .BQ.confirmed_level   = GPIO_LOW, .BQ.detected  = false, .BQ.confirmed  = false, .BQ.timer  = 0};
 
-// All of these sensors are active LOW, so only set to true on the FALLING EDGE
+
+// Common ISR
+static void sensor_gpio_isr(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+
+    switch (gpio_num)
+    {
+        case BIH_GPIO_IN:
+            sensors.BIH.detected = true;
+            TIMER_restart(&sensors.BIH.timer);
+            break;
+
+        case BIG_GPIO_IN:
+            sensors.BIG.detected = true;
+            TIMER_restart(&sensors.BIG.timer);
+            break;
+
+        case BD_GPIO_IN:
+            sensors.BD.detected = true;
+            TIMER_restart(&sensors.BD.timer);
+            break;
+
+        case BQ_GPIO_IN:
+            sensors.BQ.detected = true;
+            TIMER_restart(&sensors.BQ.timer);
+            break;
+    }
+}
+
+void generic_gpio_debounce_read(GpioSensor_t* sensor)
+{
+    if (sensor->detected && !sensor->confirmed)
+    {
+        int gpio_level = gpio_get_level(sensor->gpio);
+
+        if (gpio_level == sensor->confirmed_level)
+        {
+            if (TIMER_get_ms(sensor->timer) > DEBOUNCE_DELAY_MS)
+            {
+                sensor->confirmed = true;
+            }
+        }
+        else
+        {
+            sensor->detected = false;
+        }
+    }
+}
+
 void check_ball_in_hole(void)
 {
-
+    generic_gpio_debounce_read(&sensors.BIH);
 }
 
 void check_ball_in_gutter(void)
 {
-
+    generic_gpio_debounce_read(&sensors.BIG);
 }
 
 void check_ball_dep(void)
 {
-
+    generic_gpio_debounce_read(&sensors.BD);
 }
 
 void check_ball_queue(void)
 {
-
+    generic_gpio_debounce_read(&sensors.BQ);
 }
-
-
 
 void SNS_init(void)
 {
+    // initially read all gpios and make sure its accurate
 
 }
 
 bool SNS_get_ball_in_hole(void)
 {
-    return sensors.ballInHole;
+    return sensors.BIH_confirmed;
 }
 
 void SNS_clear_ball_in_hole(void)
 {
-    sensors.ballInHole = false;
+    sensors.BIH_confirmed = false;
+    sensors.BIH_detected = false;
 }
 
 bool SNS_get_ball_in_gutter(void)
 {
-    return sensors.ballInGutter;
+    return sensors.BIG_confirmed;
 }
 
 void SNS_clear_ball_in_gutter(void)
 {
-    sensors.ballInGutter = false;
+    sensors.BIG_confirmed = false;
+    sensors.BIG_detected = false;
 }
 
 bool SNS_get_ball_dep(void)
 {
-    return sensors.ballDep;
+    return sensors.BD_confirmed;
 }
 
 void SNS_clear_ball_dep(void)
 {
-    sensors.ballDep = false;
-}
+    sensors.BD_confirmed = false;
+    sensors.BD_detected = false;
+    }
 
 bool SNS_get_ball_queue(void)
 {
-    return sensors.ballQueue;
+    return sensors.BQ_confirmed;
 }
 
 void SNS_clear_ball_queue(void)
 {
-    sensors.ballQueue;
+    sensors.BQ_confirmed = false;
+    sensors.BQ_detected = false;
 }
 
 void SNS_run_task(void)
