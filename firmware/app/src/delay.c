269acafec699b3b41880bac957067be3926906ef
chris <tps90067@gmail.com>
2025-01-22 21:59:20 -0500

    add macro for delay

--- firmware/app/src/delay.c
+++ firmware/app/src/delay.c
@@ -1,6 +1,8 @@
 #include "delay.h"
 #include "esp_timer.h"
 
+#define US_TO_MS 1000
+
 int64_t TIMER_restart()
 {
     return esp_timer_get_time();
@@ -8,7 +10,7 @@ int64_t TIMER_restart()
 
 int64_t TIMER_get_ms(Timer_t timer)
 {
-    return round((esp_timer_get_time() - timer) / 1000.0f);
+    return round((esp_timer_get_time() - timer) / US_TO_MS);
 }
 
 int64_t TIMER_get_us(Timer_t timer)
