8e42a9cc5e54d2800e5525b550eacf4cf094b705
chris <tps90067@gmail.com>
2025-02-17 15:03:26 -0500

    Threads running, I think, LED blinks, compiles, untested for functionality

--- firmware/app/src/gpio.c
+++ firmware/app/src/gpio.c
@@ -10,6 +10,8 @@ gpio_config_t gpios[] =
     /* BD Laser */      {.intr_type = GPIO_INTR_NEGEDGE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_12), .pull_down_en = 0, .pull_up_en = 0},
     /* BQ Laser */      {.intr_type = GPIO_INTR_NEGEDGE, .mode = GPIO_MODE_INPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_13), .pull_down_en = 0, .pull_up_en = 0},
 
+    /* LED */           {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT,  .pin_bit_mask = PIN_MASK(GPIO_NUM_15), .pull_down_en = 0, .pull_up_en = 0},
+
     /* Laser Emitter */ {.intr_type = GPIO_INTR_DISABLE, .mode = GPIO_MODE_OUTPUT, .pin_bit_mask = PIN_MASK(GPIO_NUM_16), .pull_down_en = 0, .pull_up_en = 0},
 };
 
--- firmware/app/src/main.c
+++ firmware/app/src/main.c
@@ -19,9 +19,9 @@ static void task_100ms(void* arg);
 void app_main()
 {
     WIFI_init_and_start_server();
+
     I2C_master_init();
     GPIO_init();
-    
     AC_init();
     BQ_init();
     SNS_init();
@@ -29,35 +29,51 @@ void app_main()
     xTaskCreate(task_1ms,   "task_1ms",   2048, NULL, 10, NULL);
     xTaskCreate(task_10ms,  "task_10ms",  2048, NULL, 10, NULL);
     xTaskCreate(task_100ms, "task_100ms", 2048, NULL, 10, NULL);
+
+    while (1) {
+        gpio_set_level(15, !gpio_get_level(15)); // Toggle GPIO15
+        vTaskDelay(pdMS_TO_TICKS(250)); // Wait 500ms
+    }
 }
 
 static void task_1ms(void* arg)
 {
+    TickType_t xLastWakeTime = xTaskGetTickCount(); // Get current tick count
+    const TickType_t xFrequency = pdMS_TO_TICKS(1); // Convert 1ms to ticks
+    
     for (;;)
     {
         SNS_run_task();
 
-        vTaskDelay(1 / portTICK_RATE_MS);
+
+
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
 }
 
 static void task_10ms(void* arg)
 {
+    TickType_t xLastWakeTime = xTaskGetTickCount(); // Get current tick count
+    const TickType_t xFrequency = pdMS_TO_TICKS(10); // Convert 1ms to ticks
+
     for (;;)
     {
         AC_run_task();
         BE_run_task();
 
-        vTaskDelay(10 / portTICK_RATE_MS);
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
 }
 
 static void task_100ms(void* arg)
 {
+    TickType_t xLastWakeTime = xTaskGetTickCount(); // Get current tick count
+    const TickType_t xFrequency = pdMS_TO_TICKS(100); // Convert 1ms to ticks
+
     for (;;)
     {
         BQ_run_task();
 
-        vTaskDelay(100 / portTICK_RATE_MS);
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
 }
